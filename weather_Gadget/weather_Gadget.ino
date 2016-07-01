1e980471f4719a45cb0011eb3ff57275c1820ff6
siddhesh13 <sidhu.m13@gmail.com>
2016-07-01 18:51:35 +0530

    Create weather_Gadget.ino

---
+++ weather_Gadget/weather_Gadget.ino
@@ -0,0 +1,136 @@
+#include "U8glib.h"
+#include <ELClient.h>
+#include <ELClientRest.h>
+char buff[32];
+
+// replace with your channel's thingspeak API key
+String API_KEY = "15373f8c0b06b6e66e6372db065c4e46";
+String CityID = "1275339"; //Mumbai, India
+
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE|U8G_I2C_OPT_DEV_0);
+// Initialize a connection to esp-link using the normal hardware serial port both for
+// SLIP and for debug messages.
+ELClient esp(&Serial, &Serial);
+
+// Initialize a REST client on the connection to esp-link
+ELClientRest rest(&esp);
+
+boolean wifiConnected = false;
+
+// Callback made from esp-link to notify of wifi status changes
+// Here we print something out and set a global flag
+void wifiCb(void *response) 
+    {
+      ELClientResponse *res = (ELClientResponse*)response;
+      if (res->argc() == 1) 
+         {
+           uint8_t status;
+           res->popArg(&status, 1);
+
+           if(status == STATION_GOT_IP) 
+             {
+               Serial.println("WIFI CONNECTED");  //Wifi gets connected at this place
+               wifiConnected = true;
+             } 
+           else 
+             {
+               Serial.print("WIFI NOT READY: ");//Wifi not connected,check connection
+               Serial.println(status);
+               wifiConnected = false;
+             }
+         }
+    }
+
+void setup() 
+    {
+      Serial.begin(115200);   // the baud rate here needs to match the esp-link config
+      Serial.println("EL-Client starting!");
+      u8g.setFont(u8g_font_timB14);
+      u8g.setColorIndex(1);
+      // Sync-up with esp-link, this is required at the start of any sketch and initializes the
+      // callbacks to the wifi status change callback. The callback gets called with the initial
+      // status right after Sync() below completes.
+      esp.wifiCb.attach(wifiCb); // wifi status change callback, optional (delete if not desired)
+      bool ok;
+      do 
+       {
+         ok = esp.Sync();      // sync up with esp-link, blocks for up to 2 seconds
+         if (!ok) Serial.println("EL-Client sync failed!");
+       } while(!ok);
+      Serial.println("EL-Client synced!");
+
+      // Get immediate wifi status info for demo purposes. This is not normally used because the
+      // wifi status callback registered above gets called immediately. 
+      esp.GetWifiStatus();
+      ELClientPacket *packet;
+      if((packet=esp.WaitReturn()) != NULL) 
+        {
+          Serial.print("Wifi status: ");
+          Serial.println(packet->value);
+        }
+
+      // Set up the REST client to talk to api.openweathermap.org, this doesn't connect to that server,
+      // it just sets-up stuff on the esp-link side
+      int err = rest.begin("api.openweathermap.org");
+      if(err != 0) 
+        {
+          Serial.print("REST begin failed: ");
+          Serial.println(err);
+          while(1) ;
+        }
+      Serial.println("EL-REST ready");
+     
+      
+    }
+
+void loop() 
+    {
+     esp8266_TEST();
+     delay(5000);
+    }
+ 
+void showMessageOnLcd(int x,int y, const char* message1,int a,int b, const char* message2)
+    {u8g.firstPage();
+      do { //u8g.setFont(u8g_font_timB14);
+           u8g.drawStr( x, y, message1);
+          u8g.drawStr( a, b, message2);    
+         } while( u8g.nextPage() );
+    }
+    
+void esp8266_TEST()
+    { 
+      sprintf(buff, "/data/2.5/weather?id=%s&appid=%s",CityID.c_str(),API_KEY.c_str());
+           // process any callbacks coming from esp_link
+      esp.Process();
+
+     
+      // if we're connected make an HTTP request
+      if(wifiConnected) 
+        {
+          // Request /utc/now from the previously set-up server
+          rest.get((const char*)buff);
+
+          char response[512];
+          uint16_t code = rest.waitResponse(response, 512);
+          if(code == HTTP_STATUS_OK)     //check for response for HTTP request  
+            {
+             Serial.println("ARDUINO: GET successful:");
+             Serial.println(response);
+             u8g.firstPage();
+           do {  
+                u8g.setPrintPos(15, 30);
+                u8g.print(response);
+              } while( u8g.nextPage() );
+            } 
+          else 
+            {
+             Serial.print("ARDUINO: GET failed: ");
+             Serial.println(code);
+            }
+          delay(3000);
+        }
+        
+    }   
+    
+   
+   

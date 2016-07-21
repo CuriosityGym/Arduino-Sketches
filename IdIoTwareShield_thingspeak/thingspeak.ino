2c5a3077833480893912434e3e91c1b03ce3c428
siddhesh13 <sidhu.m13@gmail.com>
2016-07-21 17:34:40 +0530

    Update thingspeak.ino

--- IdIoTwareShield_thingspeak/thingspeak.ino
+++ IdIoTwareShield_thingspeak/thingspeak.ino
@@ -27,7 +27,7 @@ unsigned long samplingTime =300;  //this variable is interval(in Seconds) at whi
 unsigned long duration = 2460;     //this variable is duration(in Minutes) which is the total time for which you want to log data.
 
 // replace with your channel's thingspeak API key
-String API_KEY = "BNOJ3CIAYB2DJPDT";
+String API_KEY = "XXXXXXXXXXXXXXXX";
 // Initialize a connection to esp-link using the normal hardware serial port both for
 // SLIP and for debug messages.
 ELClient esp(&Serial, &Serial);

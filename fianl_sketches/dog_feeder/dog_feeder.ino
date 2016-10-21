540f503ff7f4e108f20b19c51f6aa06d247705a0
siddhesh13 <sidhu.m13@gmail.com>
2016-10-21 16:20:05 +0530

    Update dog_feeder.ino

--- fianl_sketches/dog_feeder/dog_feeder.ino
+++ fianl_sketches/dog_feeder/dog_feeder.ino
@@ -10,7 +10,7 @@
     Blynk community:            http://community.blynk.cc 
   
     To send data from Blynk to arduino or get data from arduino to blynk app you need to install Blynk app on your 
-    smartphone, create account to log in to the app then you will get a unique token number use thet token number in 
+    smartphone, create account to log in to the app then you will get a unique token number then use that token number in 
     this code to create your own project.
     
     In this example we have used ESP8266_Lib.h and BlynkSimpleShieldEsp8266.h  library to make communication between arduino

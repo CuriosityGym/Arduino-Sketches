e202c63c50397488658ed0d1fb876496b414660b
siddhesh13 <sidhu.m13@gmail.com>
2016-10-24 14:11:21 +0530

    Update tweet_lamp.ino

--- fianl_sketches/tweet_lamp/tweet_lamp.ino
+++ fianl_sketches/tweet_lamp/tweet_lamp.ino
@@ -1,28 +1,21 @@
-/**************************************************************
- * Blynk is a platform with iOS and Android apps to control
- * Arduino, Raspberry Pi and the likes over the Internet.
- * You can easily build graphic interfaces for all your
- * projects by simply dragging and dropping widgets.
- *
- *   Downloads, docs, tutorials: http://www.blynk.cc
- *   Blynk community:            http://community.blynk.cc
- *   Social networks:            http://www.fb.com/blynkapp
- *                               http://twitter.com/blynk_app
- *
- * Blynk library is licensed under MIT license
- * This example code is in public domain.
- *
- **************************************************************
- * Control a color gradient on NeoPixel strip using a slider!
- *
- * For this example you need NeoPixel library:
- *   https://github.com/adafruit/Adafruit_NeoPixel
- *
- * App project setup:
- *   Slider widget (0...500) on V1
- *
- **************************************************************/
-
+/*
+    idIoTWare Shield Twitter Lamp
+    Attention: Please install all libraries from our Github Repository to enable this example to run.
+    
+    In this example we are using ESP8266-01 Wifi Module  and onboard WS2812 multicolor led.
+    
+    In this example we are using Blynk HTTP RESTful API. 
+    Blynk HTTP RESTful API allows to easily read and write values to/from Pins in Blynk apps and 
+    Hardware (microcontrollers and microcomputers like Arduino, Raspberry Pi, ESP8266, Particle, etc.).
+    Every PUT request will update Pin's state both in apps and on the hardware. Every GET request will 
+    return current state/value on the given Pin. We also provide simplified API so you can do updates via GET requests.
+    You can read on how to do it here //http://docs.blynkapi.apiary.io/#reference/0/get-pin-value/write-pin-value-via-get?console=1
+    Use your Auth Token number in url to get data from server.
+    
+    In this example we have used ESP8266_Lib.h and BlynkSimpleShieldEsp8266.h  library to make communication between arduino
+    and Blynk HTTP RESTful API. We have used Heroku Deployment Application to receive tweet from our twitter handle and then this application 
+    sends get request to Blynk HTTP RESTful API.   
+*/
 
 #define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
 #include <ESP8266_Lib.h>
@@ -37,12 +30,12 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, PIN, NEO_GRB + NEO_KHZ800);
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "8ae41dd66020477a85c1b23d63e22203";
+char auth[] = "xxxxxxxxxxxxxxxxxxxxxx";  //replace it with your token number
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "CuriosityGym-BCK";
-char pass[] = "#3Twinkle3#";
+char ssid[] = "abcd";    
+char pass[] = "***********";
 
 // Hardware Serial on Mega, Leonardo, Micro...
 #define EspSerial Serial
@@ -70,12 +63,14 @@ void setup()
       strip.show();
     }
 //http://blynk-cloud.com/8ae41dd66020477a85c1b23d63e22203/update/V0?value=250?pin=V0&value=0&pin=V0&value=0
+
+//create virtual pin 1 and read value from that pin
 BLYNK_WRITE(V0)
            {
              int red = param[0].asInt();
              int green = param[1].asInt();
              int blue = param[2].asInt();
-             strip.setPixelColor(i,red,green,blue);     
+             strip.setPixelColor(0,red,green,blue); //Set color received from twitter handle 
              strip.show();
            }
 
@@ -85,4 +80,6 @@ BLYNK_WRITE(V0)
 void loop()
 {
   Blynk.run();
-}       
+}
+
+

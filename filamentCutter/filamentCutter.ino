cedc69699f60fd64b5aa205f412ef32df6dd5607
siddhesh13 <sidhu.m13@gmail.com>
2017-08-17 19:25:43 +0530

    Update filamentCutter.ino

--- filamentCutter/filamentCutter.ino
+++ filamentCutter/filamentCutter.ino
@@ -1,5 +1,4 @@
-#define EVENTS 3
-// dc motor variables
+
 int endStop = 2;
 int touchPad = 5;
 int motorPin1 = 11;
@@ -23,6 +22,7 @@ int ms1= 9;
 int ms2 = 8;
 int ms3 = 7;
 boolean extrudeFilament = false;
+boolean extrudeFilament1 = false;
 int meter = 23200;  //23200 
 int halfMeter = 11600;  //11600
 //button counter variables
@@ -37,7 +37,11 @@ int BUTTON_COUNTER = 0;
 int IDLE_DELAY=5000;
 int lastPressedMillis=0;
 
-int finalCount = 0;
+int count = 0;
+int z = 1;
+int a = 1;
+//boolean extrudeFilament = false;
+//int finalCount = 0;
 unsigned long currentMillis2 = 0;
 unsigned long previousMillis2 = 0;
 boolean checkCount = false;
@@ -81,8 +85,8 @@ void loop()
       int touchpadState = digitalRead(touchPad);
       int buttonState = digitalRead(BUTTON_PIN);
       // print out the state of the button:
-     // Serial.print("endstopState: ");
-     // Serial.println(endstopState);
+      Serial.print("endstopState: ");
+      Serial.println(endstopState);
      // Serial.print("touch Pad: ");
      // Serial.println(touchpadState);
     //  delay(1);        // delay in between reads for stability\
@@ -102,22 +106,25 @@ void loop()
         }    
      
    */  
-          if(touchpadState == 0 && buttonState == 0)
-            {  
-              
-            //  for(int i=0; i<50; i++)
-              //  { 
-               digitalWrite(ms1,HIGH);
-               digitalWrite(ms2,LOW);
-               digitalWrite(dirPin,HIGH); //Changes the rotations direction
- 
-                // Makes 400 pulses for making two full cycle rotation
-                for(int x = 0; x <= meter; x++)
-                   { 
-                    digitalWrite(buzzerPin,HIGH);
-                    delay(250);
-                    digitalWrite(buzzerPin,LOW);
-                    // Serial.println("Extrude 1 meter filament ");
+      if(touchpadState == 0 && buttonState == 0)
+        {  
+          digitalWrite(buzzerPin,HIGH);
+          delay(250);
+          digitalWrite(buzzerPin,LOW);
+          extrudeFilament1 = true;
+        }
+       while(extrudeFilament1 == true && count < 50)
+            {
+             if(a!=count)
+               {       
+                 digitalWrite(ms1,HIGH);
+                 digitalWrite(ms2,LOW);
+                 digitalWrite(dirPin,HIGH); //Changes the rotations direction
+  
+                 // Makes 400 pulses for making two full cycle rotation
+                 for(int x = 0; x <= meter; x++)
+                    { 
+                     // Serial.println("Extrude 1 meter filament ");
                      digitalWrite(stepPin,HIGH);
                      delayMicroseconds(500);
                      digitalWrite(stepPin,LOW);
@@ -127,95 +134,140 @@ void loop()
                         //extrudeFilament = false;
                         startMotor = true;
                         Serial.println("cut 1 meter filament ");
+                        a=count; 
                        }
                    }  
-             } 
-          if(touchpadState == 0 && buttonState == 1)
-            {
-              digitalWrite(buzzerPin,HIGH);
-              delay(250);
-              digitalWrite(buzzerPin,LOW);
-              digitalWrite(ms1,HIGH);
-              digitalWrite(ms2,LOW);
-              digitalWrite(dirPin,HIGH); //Changes the rotations direction
- 
-              // Makes 400 pulses for making two full cycle rotation
-              for(int y = 0; y <= halfMeter; y++)
-                 { 
-                  // Serial.println("Extrude 2 meter filament ");
-                   digitalWrite(stepPin,HIGH);
-                   delayMicroseconds(500);
-                   digitalWrite(stepPin,LOW);
-                   delayMicroseconds(500);
-                   if(y >= halfMeter)
-                       {
-                       // extrudeFilament = false;
-                         startMotor = true;
-                         Serial.println("cut haalf meter filament ");
-                       }
-                 }     
-            }
+                } 
+             if(start == true && startMotor == true)
+                {
+                  currentMillis = millis();
+                  if(pM == true)
+                    {
+                      previousMillis = currentMillis;
+                      pM = false;
+                    }
+                  right();
+                  Serial.println(" Cut Filament");
+                  endstopState = digitalRead(endStop);
+                  if(endstopState == 0)
+                    { 
+                     Serial.println(" Stop the motor");
+                     stopMotor();
+                     timeStamp = currentMillis - previousMillis;
+                     start = false;
+                     startMotor = false;
+                     pM = true;  
+                    }
+                 } 
+           if(start == false)
+             {
+              currentMillis1 = millis();
+              if(pM1 == true)
+                {
+                  previousMillis1 = currentMillis1;
+                  pM1 = false;
+                }
+              left();
+              Serial.println("Reverse the motor");
+              if((currentMillis1 - previousMillis1) > timeStamp)
+                {
+                  Serial.println(" Stop the motor again");
+                  stopMotor();
+                  start = true;
+                  pM1 = true;
+                  count++;
+                 if(count == 50)
+                   {
+                     count = 0;
+                     extrudeFilament1 = false;
+                     playTone();
+                   }  
+                }
+           }
+    }   
+   if(touchpadState == 0 && buttonState == 1)
+     {
+      digitalWrite(buzzerPin,HIGH);
+      delay(250);
+      digitalWrite(buzzerPin,LOW);
+      extrudeFilament = true;
+     }
+   while(extrudeFilament == true && count < 50)
+       {  
+        digitalWrite(ms1,HIGH);
+        digitalWrite(ms2,LOW);
+        digitalWrite(dirPin,HIGH); //Changes the rotations direction
+        if(z!=count)
+          {
+            // Makes 400 pulses for making two full cycle rotation
+            for(int y = 0; y <= halfMeter; y++)
+               { 
+                 // Serial.println("Extrude 2 meter filament ");
+                 digitalWrite(stepPin,HIGH);
+                 delayMicroseconds(500);
+                 digitalWrite(stepPin,LOW);
+                 delayMicroseconds(500);
+                 if(y >= halfMeter)
+                   {
+                     // extrudeFilament = false;
+                     startMotor = true;
+                     Serial.println("cut half meter filament ");
+                     z=count;  
+                   }
+                           
+               }     
+           }
           
-       
-  
-
-      if(start == true && startMotor == true)
-        {
-          currentMillis = millis();
-          if(pM == true)
+          if(start == true && startMotor == true)
             {
-              previousMillis = currentMillis;
-              pM = false;
+             currentMillis = millis();
+             if(pM == true)
+               {
+                previousMillis = currentMillis;
+                pM = false;
+               }
+             right();
+             Serial.println(" Cut Filament");
+             endstopState = digitalRead(endStop);
+             if(endstopState == 0)
+               { 
+                Serial.println(" Stop the motor");
+                stopMotor();
+                timeStamp = currentMillis - previousMillis;
+                start = false;
+                startMotor = false;
+                pM = true;
+            
+               }
             }
-          right();
-          Serial.println(" Cut Filament");
-          if(endstopState == 0)
-            { 
-              Serial.println(" Stop the motor");
-              stopMotor();
-              timeStamp = currentMillis - previousMillis;
-              start = false;
-              startMotor = false;
-              pM = true;
-              
-           
+          if(start == false)
+            {
+             currentMillis1 = millis();
+             if(pM1 == true)
+               {
+                previousMillis1 = currentMillis1;
+                pM1 = false;
+               }
+             left();
+             Serial.println("Reverse the motor");
+             if((currentMillis1 - previousMillis1) > timeStamp)
+               {
+                 Serial.println(" Stop the motor again");
+                 stopMotor();
+                 start = true;
+                 pM1 = true;
+                 count++;
+                 if(count == 50)
+                   {
+                    count = 0;
+                    extrudeFilament = false;
+                    playTone();
+                   }    
+                }
             }
-        }
-     if(start == false)
-       {
-         currentMillis1 = millis();
-         if(pM1 == true)
-           {
-             previousMillis1 = currentMillis1;
-             pM1 = false;
-           }
-         left();
-         Serial.println("Reverse the motor");
-         if((currentMillis1 - previousMillis1) > timeStamp)
-           {
-             Serial.println(" Stop the motor again");
-             stopMotor();
-             start = true;
-             pM1 = true;
-             for (int thisNote = 0; thisNote < 8; thisNote++) {
-
-              // to calculate the note duration, take one second
-              // divided by the note type.
-              //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-              int noteDuration = 1000 / noteDurations[thisNote];
-              tone(13, melody[thisNote], noteDuration);
-
-              // to distinguish the notes, set a minimum time between them.
-              // the note's duration + 30% seems to work well:
-              int pauseBetweenNotes = noteDuration * 1.30;
-              delay(pauseBetweenNotes);
-               // stop the tone playing:
-              noTone(13);
-             }
-           }
-        }
-           // }
-    }
+                 
+       }
+   }
 
 int right()
    {
@@ -234,4 +286,21 @@ int stopMotor()
     digitalWrite(motorPin1, LOW);
     digitalWrite(motorPin2, LOW);
    }   
+void playTone()
+    {
+       for (int thisNote = 0; thisNote < 8; thisNote++) 
+            {
+               // to calculate the note duration, take one second
+               // divided by the note type.
+               //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+               int noteDuration = 1000 / noteDurations[thisNote];
+               tone(13, melody[thisNote], noteDuration);
 
+               // to distinguish the notes, set a minimum time between them.
+               // the note's duration + 30% seems to work well:
+               int pauseBetweenNotes = noteDuration * 1.30;
+               delay(pauseBetweenNotes);
+               // stop the tone playing:
+               noTone(13);
+            }                   
+    }

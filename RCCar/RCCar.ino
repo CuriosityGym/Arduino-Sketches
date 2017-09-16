61f8d14a1e3a519b09374215d6b1018fa10b5bbc
RupinC <rupin@curiositygym.com>
2017-09-16 12:35:51 +0530

    RC Car with Servo Mounted Ultrasonic Sensor

---
+++ RCCar/RCCar.ino
@@ -0,0 +1,288 @@
+#include <Servo.h>
+#include <NewPing.h>
+Servo devil;
+
+int obs_dis_90;
+int obs_dis_60;
+int obs_dis_120;
+
+
+
+
+
+#define TRIGGER_PIN  A1  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     A0  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
+#define MOTORDELAY 500
+
+
+
+// Arduino pins for the shift register
+#define MOTORLATCH 12
+#define MOTORCLK 4
+#define MOTORENABLE 7
+#define MOTORDATA 8
+
+// 8-bit bus after the 74HC595 shift register
+// (not Arduino pins)
+// These are used to set the direction of the bridge driver.
+#define MOTOR1_A 2
+#define MOTOR1_B 3
+#define MOTOR2_A 1
+#define MOTOR2_B 4
+#define MOTOR3_A 5
+#define MOTOR3_B 7
+#define MOTOR4_A 0
+#define MOTOR4_B 6
+
+// Arduino pins for the PWM signals.
+#define MOTOR1_PWM 11
+#define MOTOR2_PWM 3
+#define MOTOR3_PWM 6
+#define MOTOR4_PWM 5
+#define SERVO1_PWM 10
+#define SERVO2_PWM 9
+
+// Codes for the motor function.
+#define FORWARD 1
+#define BACKWARD 2
+#define BRAKE 3
+#define RELEASE 4
+
+int getdistance(int angle)
+{
+  devil.write(angle);
+  delay(500);
+  return sonar.ping_cm();
+}
+
+void setup() {
+  delay(3000);
+  devil.attach(9);
+  Serial.begin(115200);
+}
+
+void loop() {
+  forward();
+  delay(MOTORDELAY);
+
+ brake();
+  
+  obs_dis_60 = getdistance(60);
+  obs_dis_90 = getdistance(90);
+  obs_dis_120 = getdistance(120);
+
+
+
+
+
+  if (obs_dis_90 < 20)
+  {
+    back();
+    delay(MOTORDELAY);
+    leftspin();
+    delay(MOTORDELAY);
+  }
+  else if (obs_dis_60 > 20)
+  {
+
+    forward();
+    delay(MOTORDELAY);
+  }
+  else if (obs_dis_120 < 20)
+  {
+    back();
+    delay(MOTORDELAY);
+    rightspin();
+    delay(MOTORDELAY);
+  }
+
+
+}
+
+void forward()
+{
+  motor(2, FORWARD, 255);
+  motor(3, FORWARD, 255);
+}
+
+
+void back()
+{
+  motor(2, BACKWARD, 255);
+  motor(3, BACKWARD, 255);
+}
+void leftspin()
+{
+  motor(2, BACKWARD, 255);
+  motor(3, FORWARD, 255);
+}
+void rightspin()
+{
+  motor(2, FORWARD, 255);
+  motor(3, BACKWARD, 255);
+}
+
+void brake()
+{
+  motor (2, BRAKE, 0);
+  motor (3, BRAKE, 0);
+}
+
+
+//void shiftWrite(int output, int high_low);
+//void motor_output (int output, int high_low, int speed);
+//void motor(int nMotor, int command, int speed);
+void shiftWrite(int output, int high_low)
+{
+  static int latch_copy;
+  static int shift_register_initialized = false;
+
+  // Do the initialization on the fly,
+  // at the first time it is used.
+  if (!shift_register_initialized)
+  {
+    // Set pins for shift register to output
+    pinMode(MOTORLATCH, OUTPUT);
+    pinMode(MOTORENABLE, OUTPUT);
+    pinMode(MOTORDATA, OUTPUT);
+    pinMode(MOTORCLK, OUTPUT);
+
+    // Set pins for shift register to default value (low);
+    digitalWrite(MOTORDATA, LOW);
+    digitalWrite(MOTORLATCH, LOW);
+    digitalWrite(MOTORCLK, LOW);
+    // Enable the shift register, set Enable pin Low.
+    digitalWrite(MOTORENABLE, LOW);
+
+    // start with all outputs (of the shift register) low
+    latch_copy = 0;
+
+    shift_register_initialized = true;
+  }
+
+  // The defines HIGH and LOW are 1 and 0.
+  // So this is valid.
+  bitWrite(latch_copy, output, high_low);
+
+  // Use the default Arduino 'shiftOut()' function to
+  // shift the bits with the MOTORCLK as clock pulse.
+  // The 74HC595 shiftregister wants the MSB first.
+  // After that, generate a latch pulse with MOTORLATCH.
+  shiftOut(MOTORDATA, MOTORCLK, MSBFIRST, latch_copy);
+  delayMicroseconds(5);    // For safety, not really needed.
+
+  digitalWrite(MOTORLATCH, HIGH);
+  delayMicroseconds(5);    // For safety, not really needed.
+  digitalWrite(MOTORLATCH, LOW);
+}
+
+
+void motor_output (int output, int high_low, int speed)
+{
+  int motorPWM;
+
+  switch (output)
+  {
+    case MOTOR1_A:
+    case MOTOR1_B:
+      motorPWM = MOTOR1_PWM;
+      break;
+    case MOTOR2_A:
+    case MOTOR2_B:
+      motorPWM = MOTOR2_PWM;
+      break;
+    case MOTOR3_A:
+    case MOTOR3_B:
+      motorPWM = MOTOR3_PWM;
+      break;
+    case MOTOR4_A:
+    case MOTOR4_B:
+      motorPWM = MOTOR4_PWM;
+      break;
+    default:
+      // Use speed as error flag, -3333 = invalid output.
+      speed = -3333;
+      break;
+  }
+
+  if (speed != -3333)
+  {
+    // Set the direction with the shift register
+    // on the MotorShield, even if the speed = -1.
+    // In that case the direction will be set, but
+    // not the PWM.
+    shiftWrite(output, high_low);
+
+    // set PWM only if it is valid
+    if (speed >= 0 && speed <= 255)
+    {
+      analogWrite(motorPWM, speed);
+    }
+  }
+}
+
+
+void motor(int nMotor, int command, int speed)
+{
+  int motorA, motorB;
+
+  if (nMotor >= 1 && nMotor <= 4)
+  {
+    switch (nMotor)
+    {
+      case 1:
+        motorA   = MOTOR1_A;
+        motorB   = MOTOR1_B;
+        break;
+      case 2:
+        motorA   = MOTOR2_A;
+        motorB   = MOTOR2_B;
+        break;
+      case 3:
+        motorA   = MOTOR3_A;
+        motorB   = MOTOR3_B;
+        break;
+      case 4:
+        motorA   = MOTOR4_A;
+        motorB   = MOTOR4_B;
+        break;
+      default:
+        break;
+    }
+
+    switch (command)
+    {
+      case FORWARD:
+        motor_output (motorA, HIGH, speed);
+        motor_output (motorB, LOW, -1);     // -1: no PWM set
+        break;
+      case BACKWARD:
+        motor_output (motorA, LOW, speed);
+        motor_output (motorB, HIGH, -1);    // -1: no PWM set
+        break;
+      case BRAKE:
+        // The AdaFruit library didn't implement a brake.
+        // The L293D motor driver ic doesn't have a good
+        // brake anyway.
+        // It uses transistors inside, and not mosfets.
+        // Some use a software break, by using a short
+        // reverse voltage.
+        // This brake will try to brake, by enabling
+        // the output and by pulling both outputs to ground.
+        // But it isn't a good break.
+        motor_output (motorA, LOW, 255); // 255: fully on.
+        motor_output (motorB, LOW, -1);  // -1: no PWM set
+        break;
+      case RELEASE:
+        motor_output (motorA, LOW, 0);  // 0: output floating.
+        motor_output (motorB, LOW, -1); // -1: no PWM set
+        break;
+      default:
+        break;
+    }
+  }
+}

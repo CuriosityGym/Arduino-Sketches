dd2747ad354ce03cd4a5b893590459ad8ca05670
siddheshmurudkar <32839650+siddheshmurudkar@users.noreply.github.com>
2018-07-29 16:59:45 +0530

    Create tempControl.ino

---
+++ homeAuto_iHub/tempControl.ino
@@ -0,0 +1,60 @@
+#include "DHT.h"
+
+#define DHTPIN 2     // what digital pin we're connected to
+
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11   // DHT 11
+
+// Initialize DHT sensor.
+// Note that older versions of this library took an optional third parameter to
+// tweak the timings for faster processors.  This parameter is no longer needed
+// as the current DHT reading algorithm adjusts itself to work on faster procs.
+DHT dht(DHTPIN, DHTTYPE);
+
+int fanPin = 6;
+void setup() {
+  // put your setup code here, to run once:
+    Serial.begin(9600);
+  Serial.println("DHTxx test!");
+  pinMode(fanPin, OUTPUT);
+  dht.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+ 
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+
+  if(t > 26.00 )
+  {
+    digitalWrite(fanPin, HIGH);
+    
+  }
+ 
+if(t <= 26.00 )
+  {
+    digitalWrite(fanPin, LOW);
+  }
+}
